8d617c9d7a3461411d971357e18c72d619aaa506
Surya prakashlokula <59243908+suryadevelope@users.noreply.github.com>
2026-01-29 13:24:13 +0530

    Update ultrasonic.ino

--- kits-clg/ultrasonic.ino
+++ kits-clg/ultrasonic.ino
@@ -3,17 +3,28 @@
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
 
+
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+
 /* ================= WIFI DETAILS ================= */
-#define WIFI_SSID       "raspberry"
+#define WIFI_SSID       "rajasri28"
 #define WIFI_PASSWORD   "123456789"
 
 /* ================= FIREBASE DETAILS ================= */
-#define API_KEY         "AIzaSyA3NluveWnDnGu549rP_vUbWaQRIfyTS80"
-#define DATABASE_URL    "https://kitsultrasonic-default-rtdb.firebaseio.com/"
+#define API_KEY         "AIzaSyAPVFIlUk9g59BWUIoi7PASjd4rqJ4QEfs"
+#define DATABASE_URL    "https://kits-c6149-default-rtdb.firebaseio.com"
 
 /* ================= ULTRASONIC PINS ================= */
-#define TRIG_PIN 18
-#define ECHO_PIN 19
+#define TRIG_PIN 25
+#define ECHO_PIN 26
+
+
+#define USER_EMAIL "example@gmail.com"
+#define USER_PASSWORD "123456789"
 
 /* ================= VARIABLES ================= */
 long duration;
@@ -26,11 +37,6 @@ FirebaseConfig config;
 
 bool signupOK = false;
 
-/* ===== TOKEN STATUS CALLBACK ===== */
-void tokenStatusCallback(TokenInfo info) {
-  Serial.print("Token status: ");
-  Serial.println(info.status == token_status_ready ? "READY" : "NOT READY");
-}
 
 void setup() {
   Serial.begin(115200);
@@ -54,20 +60,26 @@ void setup() {
 
   /* ===== FIREBASE CONFIG ===== */
   config.api_key = API_KEY;
-  config.database_url = DATABASE_URL;
-  config.token_status_callback = tokenStatusCallback;
 
-  /* ===== ANONYMOUS SIGN-UP (CRITICAL FIX) ===== */
-  if (Firebase.signUp(&config, &auth, "", "")) {
-    Serial.println("Firebase SignUp OK");
-    signupOK = true;
-  } else {
-    Serial.print("SignUp Error: ");
-    Serial.println(config.signer.signupError.message.c_str());
-  }
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  config.database_url = DATABASE_URL;
+   config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+  Firebase.reconnectNetwork(true);
+
+  // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
+  // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+  fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+
+  // Limit the size of response payload to be collected in FirebaseData
+  fbdo.setResponseSize(2048);
 
   Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
 }
 
 void loop() {
@@ -90,7 +102,7 @@ void loop() {
     Serial.println(" cm");
 
     /* ===== FIREBASE SEND ===== */
-    if (signupOK && Firebase.ready()) {
+    if (Firebase.ready()) {
       if (Firebase.RTDB.setFloat(&fbdo, "/ultrasonic/distance_cm", distance)) {
         Serial.println("Firebase Update OK ");
       } else {
